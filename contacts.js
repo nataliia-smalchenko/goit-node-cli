e7c3b726b4c5b120bab2217378d5baf7a3cf104e
Nataliia Smalchenko <yarokrilka@gmail.com>
2024-05-25 20:37:01 +0300

    add start files

---
+++ contacts.js
@@ -0,0 +1,22 @@
+import fs from "fs/promises";
+import path from "path";
+
+const contactsPath = path.resolve("db", "contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find((contact) => contact.id === contactId);
+}
+
+async function removeContact(contactId) {
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function addContact(name, email, phone) {
+  // ...твій код. Повертає об'єкт доданого контакту (з id).
+}
